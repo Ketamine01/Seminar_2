d201e6c9015f1e88330f148600701eed8ffe75c5
Максим <fedyush90@mail.ru>
2022-09-04 22:00:41 +0300

    выходной не выходной

---
+++ dz_2_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 6 | n == 7)
+{
+    Console.Write(n + "выходной день");
+}
+else
+{
+    Console.WriteLine(n + "не выходной день");
+}
